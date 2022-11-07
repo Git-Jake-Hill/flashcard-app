c1ca935c542fdcbb0f543634aab1ac2ac430c04f
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-11-07 17:42:07 +1100

    added api call to add cards to db

--- frontend/src/Components/AddCardForm.js
+++ frontend/src/Components/AddCardForm.js
@@ -1,12 +1,36 @@
-function AddCardForm() {
+import { useState } from "react";
+import axios from "axios";
+
+function AddCardForm(props) {
+  const curDeck = props.curDeck;
+  const setPost = props.setPost;
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newCard = {
+      question: question,
+      answer: answer,
+      tag: curDeck,
+    };
+
+    axios.post(`/api/new-card`, newCard);
+
+    setPost(!props.post);
+  };
+
   return (
     <div className="addCards">
       <h2 className=" cardCount mt-4">Add Cards</h2>
-      <form action="/api/new-card" method="post">
+      <form onSubmit={handleSubmit}>
         <div className="row addCard">
           <div className="col-8">
             <div className="mb-2">
               <input
+                onChange={(event) => {
+                  setQuestion(event.target.value);
+                }}
                 type="text"
                 className="form-control"
                 id="question"
@@ -16,6 +40,9 @@ function AddCardForm() {
             </div>
             <div className="mb-2">
               <input
+                onChange={(event) => {
+                  setAnswer(event.target.value);
+                }}
                 type="text"
                 className="form-control"
                 id="answer"
