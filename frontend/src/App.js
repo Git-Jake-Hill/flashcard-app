d978343dfea6cfd30f9e318470935989e33133cf
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-11-07 17:48:32 +1100

    moved state control up to app from Cards and Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,40 @@
 import Cards from "./Pages/Cards";
 import Test from "./Pages/Test";
 import About from "./Pages/About";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [cardList, setCardList] = useState([]);
+  const [curDeck, setCurDeck] = useState("algo");
+  const [reloadDeck, setReloadDeck] = useState(false);
+
+  useEffect(() => {
+    const loadCardInfo = async () => {
+      const response = await axios.get(`/api/cards/${curDeck}`);
+      const cardInfo = response.data;
+      setCardList(cardInfo);
+    };
+    loadCardInfo();
+  }, [curDeck, reloadDeck]);
+
+  const propsObj = {
+    cardList,
+    setCardList,
+    curDeck,
+    setCurDeck,
+    reloadDeck,
+    setReloadDeck,
+  };
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Test />} />
-          <Route path="cards" element={<Cards />} />
+          <Route path="/" element={<Test {...propsObj} />} />
+          <Route path="cards" element={<Cards {...propsObj} />} />
           <Route path="about" element={<About />} />
         </Routes>
       </div>
