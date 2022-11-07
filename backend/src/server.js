c12b907f9915ac73bbdf15c9519777918aa1e69d
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-11-07 17:37:07 +1100

    updated server to port 8000 for testing with react

--- backend/src/server.js
+++ backend/src/server.js
@@ -59,6 +59,6 @@ mongoose.connect(dbUrl, (err) => {
   console.log("mongoDB connection", err);
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(8000, () => {
+  console.log("Listening on port 8000");
 });
