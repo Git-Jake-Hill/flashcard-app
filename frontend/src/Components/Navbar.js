aae82dc7c1729cf09334b4e7aaa35c31a8dfa2fb
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-11-08 21:29:54 +1100

    updated navbar to show tabs

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,12 +1,48 @@
 import { Link } from "react-router-dom";
+import { BsPlayCircle } from "react-icons/bs";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+  const [navActive, setnavActive] = useState("");
+
+  useEffect(() => {
+    const curPath = window.location.href;
+    setnavActive(curPath.substr(curPath.lastIndexOf("/")));
+  }, []);
+
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="container-fluid">
-        <Link to="/">Test</Link>
-        <Link to="/cards">Cards</Link>
-        <Link to="/about">About</Link>
+    // fixed-bottom
+    <nav className="navbar navbar-dark bg-light justify-content-center">
+      <div className="nav nav-tabs">
+        <div className="nav-item">
+          <Link
+            className={navActive === "/" ? "nav-link active" : "nav-link"}
+            onClick={() => setnavActive("/")}
+            to="/"
+          >
+            {/* <BsPlayCircle /> */}
+            Test
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link
+            className={navActive === "/cards" ? "nav-link active" : "nav-link"}
+            onClick={() => setnavActive("/cards")}
+            to="/cards"
+          >
+            Cards
+          </Link>
+        </div>
+
+        <div className="nav-item">
+          <Link
+            className={navActive === "/about" ? "nav-link active" : "nav-link"}
+            onClick={() => setnavActive("/about")}
+            to="/about"
+          >
+            About
+          </Link>
+        </div>
       </div>
     </nav>
   );
