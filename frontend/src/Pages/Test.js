f8fb57f6f5889229bcce477b9600e5e7b383c876
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-11-09 11:50:58 +1100

    updated filterCardDisplay props that are passed in

--- frontend/src/Pages/Test.js
+++ frontend/src/Pages/Test.js
@@ -9,11 +9,20 @@ function Test({
   setCurDeck,
   reloadDeck,
   setReloadDeck,
+  known,
+  setKnown,
+  filterList,
 }) {
   return (
     <div className="container pt-4">
       <h1 className="display-1 text-center">Run Test</h1>
       <br />
+      <FilterCardDisplay
+        curDeck={curDeck}
+        setCurDeck={setCurDeck}
+        known={known}
+        setKnown={setKnown}
+      />
       <TestDisplay
         cardList={cardList}
         curDeck={curDeck}
