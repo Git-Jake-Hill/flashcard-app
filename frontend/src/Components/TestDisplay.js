5acaccac71cd6e94b0f5137fe4de2e35c0c67cc6
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-11-09 11:52:04 +1100

    updated TestDisplay set known functionality

--- frontend/src/Components/TestDisplay.js
+++ frontend/src/Components/TestDisplay.js
@@ -1,28 +1,76 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function TestDisplay({ cardList }) {
+function TestDisplay({
+  cardList,
+  curDeck,
+  reloadDeck,
+  setReloadDeck,
+  known,
+  filterList,
+}) {
   const [showQuestion, setShowQuestion] = useState(true);
   const [cardIndex, setCardIndex] = useState(0);
+  const [curCard, setCurCard] = useState({
+    question: "No cards to show",
+    answer: "No cards to show",
+  });
+
+  useEffect(() => {
+    console.log("===setShowQuestion:true===");
+    // set card to question if deck changes
+    setShowQuestion(true);
+  }, [curDeck, known]);
 
   const nextCard = () => {
     setShowQuestion(true);
-    if (cardIndex + 1 === cardList.length) {
+    if (filterList.length === 0) {
+      return;
+    } else if (cardIndex + 1 >= filterList.length) {
       // reset index at end of list
-      setCardIndex((cardIndex + 1) % cardList.length);
+      setCardIndex((cardIndex + 1) % filterList.length);
     } else {
       setCardIndex(cardIndex + 1);
     }
   };
-  let flipCard = showQuestion ? "question" : "answer";
-  let curQuestion = "No cards to show";
-  if (cardList.length > 0) {
-    curQuestion = cardList[cardIndex][flipCard];
-  }
+
+  const setKnown = async () => {
+    if (curCard.question !== "No cards to show") {
+      setShowQuestion(true);
+      const response = await axios
+        .put("/api/update-known", curCard)
+        .then((res) => {
+          setCurCard({
+            question: " ",
+            answer: " ",
+          });
+          setReloadDeck(!reloadDeck);
+        });
+    }
+  };
+
+  useEffect(() => {
+    if (cardIndex > filterList.length - 1) {
+      filterList.length === 0
+        ? setCardIndex(0)
+        : setCardIndex(filterList.length - 1);
+    }
+
+    filterList.length > 0 && filterList.length > cardIndex
+      ? setCurCard(filterList[cardIndex])
+      : setCurCard({
+          question: "No cards to show",
+          answer: "No cards to show",
+        });
+  }, [cardIndex, filterList, curDeck, known]);
+
   return (
     <>
-      <h4 className="cardCount mt-4">{cardList.length} Cards</h4>
+      <h4 className="cardCount mt-4">{filterList.length} Cards</h4>
       <div className="cardDisplay p-5 my-5 mt-3 border">
-        <h4>{curQuestion}</h4>
+        <h4 className={!showQuestion ? "fst-italic fw-normal" : ""}>
+          {curCard[showQuestion ? "question" : "answer"]}
+        </h4>
       </div>
       <div className="container buttonLayout">
         <div className="row">
@@ -50,7 +98,13 @@ function TestDisplay({ cardList }) {
           </div>
         </div>
         <div className="d-grid gap-2 my-4">
-          <button className="btn btn-success" type="button">
+          <button
+            className="btn btn-success"
+            type="button"
+            onClick={() => {
+              setKnown();
+            }}
+          >
             I Know it
           </button>
         </div>
