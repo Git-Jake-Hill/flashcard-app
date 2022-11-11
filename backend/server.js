238a8c098d59781e77260832174cccfe6ba36979
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-11-11 19:00:27 +1100

    added Docker for container hosting

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(__dirname + "/build"));
+console.log(__dirname);
 const dbUrl = process.env.MONGODB_URI;
 const port = 8000;
 
