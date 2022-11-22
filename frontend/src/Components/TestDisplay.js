b4792e13bb53b8a5e722437a67c1b5c3264914df
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-11-22 21:18:07 +1100

    updated test to show known card count

--- frontend/src/Components/TestDisplay.js
+++ frontend/src/Components/TestDisplay.js
@@ -8,6 +8,8 @@ function TestDisplay({
   setReloadDeck,
   known,
   filterList,
+  countTotal,
+  countKnown,
 }) {
   const [showQuestion, setShowQuestion] = useState(true);
   const [cardIndex, setCardIndex] = useState(0);
@@ -65,8 +67,14 @@ function TestDisplay({
 
   return (
     <>
-      <h4 className="cardCount mt-4">{filterList.length} Cards</h4>
-      <div className="cardDisplay p-5 my-5 mt-3 border">
+      <div className="row">
+        <h4 className="cardCount mt-3 mb-0 col">{filterList.length} Cards</h4>
+        <p className="mb-0 align-self-end col">
+          Known: {countKnown}/{countTotal}
+        </p>
+      </div>
+
+      <div className="cardDisplay px-3 py-5 mb-3 mt-2 border">
         <h4 className={!showQuestion ? "fw-normal text-center" : "text-center"}>
           {curCard[showQuestion ? "question" : "answer"]}
         </h4>
@@ -104,7 +112,7 @@ function TestDisplay({
               setKnown();
             }}
           >
-            I Know it
+            {curCard.known === true ? "Mark as Unknown" : "Mark as Known"}
           </button>
         </div>
       </div>
