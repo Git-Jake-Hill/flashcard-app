6020d8114a9253252c6cd3939f65b8ebceb02cc4
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-12-05 09:07:23 +1100

    updated whitespace formatting

--- frontend/src/Components/FilterCardDisplay.js
+++ frontend/src/Components/FilterCardDisplay.js
@@ -1,47 +1,47 @@
 function FilterCardDisplay({ curDeck, setCurDeck, known, setKnown }) {
-  return (
-    <div className="row filterCards">
-      <div className="col">
+    return (
+        <div className="row filterCards">
+            <div className="col">
                 <label
                     htmlFor="formSelectDeck"
                     className="form-label text-start"
                 >
-          Topic:
-        </label>
-        <select
-          className="form-select"
-          aria-label="Form select deck"
-          id="formSelectDeck"
-          onChange={(event) => setCurDeck(event.target.value)}
-          value={curDeck}
-        >
+                    Topic:
+                </label>
+                <select
+                    className="form-select"
+                    aria-label="Form select deck"
+                    id="formSelectDeck"
+                    onChange={(event) => setCurDeck(event.target.value)}
+                    value={curDeck}
+                >
                     {"// TODO change options to be dynamic"}
-          <option value="big o notation">bigO Notation</option>
-          <option value="git">Git</option>
-          <option value="linux">Linux</option>
-        </select>
-      </div>
-      <div className="col">
+                    <option value="big o notation">bigO Notation</option>
+                    <option value="git">Git</option>
+                    <option value="linux">Linux</option>
+                </select>
+            </div>
+            <div className="col">
                 <label
                     htmlFor="formSelectDeck"
                     className="form-label text-start"
                 >
-          Filter by:
-        </label>
-        <select
-          className="form-select"
-          aria-label="Form select deck"
-          id="formSelectDeck"
-          onChange={(event) => setKnown(event.target.value)}
-          value={known}
-        >
-          <option value="all">All</option>
-          <option value="known">Known</option>
-          <option value="unknown">Unknown</option>
-        </select>
-      </div>
-    </div>
-  );
+                    Filter by:
+                </label>
+                <select
+                    className="form-select"
+                    aria-label="Form select deck"
+                    id="formSelectDeck"
+                    onChange={(event) => setKnown(event.target.value)}
+                    value={known}
+                >
+                    <option value="all">All</option>
+                    <option value="known">Known</option>
+                    <option value="unknown">Unknown</option>
+                </select>
+            </div>
+        </div>
+    );
 }
 
 export default FilterCardDisplay;
